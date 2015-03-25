b6f9c6ae7fd078f938f0e8de97c8fb968ae13a8e
Your Name <everettchurchey@crbc.edu>
2015-03-25 11:03:37 -0400

    Initial Commit

---
+++ Conditionals/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Conditionals
+//
+//  Created by Ej Churchey on 3/25/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
